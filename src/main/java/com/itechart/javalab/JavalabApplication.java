1bcc66a81dda83c26c78b959bfd7adea50b93404
Yauhen <yauhenmalchanau@gmail.com>
2020-09-24 21:04:50 +0300

    init

---
+++ src/main/java/com/itechart/javalab/JavalabApplication.java
@@ -0,0 +1,14 @@
+package com.itechart.javalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechart/javalab/JavalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.javalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
